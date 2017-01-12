c84c332b41ab21ae0fb2cfe92532d7896d2bb156
Aaron Attarzadeh <attarzad@usc.edu>
2017-01-12 10:16:03 -0800

    adding helloworld.cpp

---
+++ helloworld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+int main() {
+	cout << "Hello World" << endl;
+	cout << "My Name is Aaron" << endl;
+	cout << "This is really Cool!" << endl;
+	cout << "This is really Cool!" << endl;
+	cout << "Test add" << endl;
+}
